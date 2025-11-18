dc6aeaab668fc0373cdf9041fc69542ad87b9861
Plater <abbosasrorov2012@gmail.com>
2025-11-18 02:12:37 -0700

    Fix formatting and remove unnecessary lines

--- main.cpp
+++ main.cpp
@@ -2,11 +2,9 @@
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     MD5Hasher app;
-    
     if (!app.initialize(hInstance)) {
         MessageBoxA(nullptr, "Failed to initialize application", "Error", MB_OK);
         return 1;
     }
-    
     return app.run();
 }
